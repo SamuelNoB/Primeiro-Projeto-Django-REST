e20c1bc5a3c9c402c58e2153627b038b44e61c29
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2020-05-26 18:20:47 -0300

    por favoooor

--- ponto_turistico/settings.py
+++ ponto_turistico/settings.py
@@ -23,10 +23,6 @@ if os.name == 'nt':
     os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
     os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
     os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
-    libgdal.lib_names.append('gdal300', 'gdal111')
-elif os.name == 'posix':
-    libgdal.lib_names.append('gdal3.0.0', 'gdal1.1.1')
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
