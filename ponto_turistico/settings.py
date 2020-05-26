b192e12b29337fadb4a50206af66c4ab0313860d
Samuel Nogueira <samuelnbacelar@hotmail.com>
2020-05-26 15:21:07 -0300

    .

--- ponto_turistico/settings.py
+++ ponto_turistico/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pontos-django-rest-api.herokuapp.com/', 'localhost:8000']
 
 
 # Application definition
