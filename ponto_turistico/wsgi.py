a1ef38fa61b246255a555a2e4eb801e97e9667a9
Samuel Nogueira Bacelar <samuelnbacelar@hotmail.com>
2020-05-26 17:03:06 -0300

    arrumando

--- ponto_turistico/wsgi.py
+++ ponto_turistico/wsgi.py
@@ -11,8 +11,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ponto_turistico.settings')
 
 application = Cling(get_wsgi_application())
